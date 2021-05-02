d7b8613902d18d1827c2bf29e414250af87e6f35
Salvation Lloyd Stanley-Kemuel <skemuel007@gmail.com>
2021-05-02 07:32:57 +0100

    Models created

---
+++ server.js
@@ -0,0 +1,24 @@
+// import packages
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const config = require('config');
+
+// call express app
+const app = express();
+app.use(express.json());
+
+// set up server to serve static file content, i.e. react
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
+
+// connecting to mongoDB and then running server on port 4000
+const dbURI = config.get('dbURI');
+const port = process.env.PORT || 3000;
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+    .then((result) => app.listen(port))
+    .catch((err) => console.log(err));
